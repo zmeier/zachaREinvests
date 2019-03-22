a13558a454184354687bc47371fd671a45275d39
Zach Meier <meier241@umn.edu>
2019-03-21 21:08:16 -0700

    Adding AWS deployment details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+const targetAddress = new URL(
+  process.env.TARGET_ADDRESS || `http://zachareinvests.local`
+)
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +29,25 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: process.env.TARGET_BUCKET_NAME || "fake-bucket",
+        region: process.env.AWS_REGION,
+        protocol: targetAddress.protocol.slice(0, -1),
+        hostname: targetAddress.hostname,
+        acl: null,
+        params: {
+          // In case you want to add any custom content types: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: targetAddress.href.slice(0, -1),
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
