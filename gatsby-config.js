68bb0adf0fc9bed2f0c4347d6f11159e29f0e675
Zach Meier <meier241@umn.edu>
2019-03-22 19:51:48 -0700

    Updating build to not manage deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,8 @@
-const targetAddress = new URL(
-  process.env.TARGET_ADDRESS || `http://zachareinvests.local`
-)
-
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `zachaREinvests.com`,
+    description: `Looking to buy land all across the United States.`,
+    author: `@zmeier`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -32,22 +28,9 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-s3`,
-      options: {
-        bucketName: process.env.TARGET_BUCKET_NAME || "fake-bucket",
-        region: process.env.AWS_REGION,
-        protocol: targetAddress.protocol.slice(0, -1),
-        hostname: targetAddress.hostname,
-        acl: null,
-        params: {
-          // In case you want to add any custom content types: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
-        },
-      },
-    },
-    {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: targetAddress.href.slice(0, -1),
+        siteUrl: "https://zachareinvests.com"
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
